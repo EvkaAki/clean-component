223286a45b9661f554583e8bc4631cef6b63439e
Eva Nedeliakova <eva.nedeliakova@ui42.sk>
2021-05-06 21:04:24 +0200

    change parameter name

--- deleteRunPods.py
+++ deleteRunPods.py
@@ -7,12 +7,14 @@ config.load_incluster_config()
 v1 = client.CoreV1Api()
 current_namespace = open("/var/run/secrets/kubernetes.io/serviceaccount/namespace").read()
 parser = argparse.ArgumentParser(description='Find and delete run pods.')
-parser.add_argument('--workflow', type=str,
+parser.add_argument('--workflow_name', type=str,
   help='Path of the local file containing the Workflow name.')
 args = parser.parse_args()
 
-workflow_name = args.workflow
+workflow_name = args.workflow_name
 pods = v1.list_namespaced_pod(current_namespace).items
+print(workflow_name)
+print(workflow_name)
 r = re.compile(".*"+workflow_name)
 pods = list(filter(r.match, pods))
 
