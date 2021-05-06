cd11574c59884c37ff60b1a070e9518e946bc0dc
Eva Nedeliakova <eva.nedeliakova@ui42.sk>
2021-05-06 21:13:01 +0200

    print logs

--- deleteRunPods.py
+++ deleteRunPods.py
@@ -13,7 +13,7 @@ args = parser.parse_args()
 
 workflow_name = args.workflow_name
 pods = v1.list_namespaced_pod(current_namespace).items
-print(workflow_name)
+print(args)
 print(workflow_name)
 r = re.compile(".*"+workflow_name)
 pods = list(filter(r.match, pods))
