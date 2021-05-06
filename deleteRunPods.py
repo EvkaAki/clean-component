a94fec3c9afec6bffee1c6c6ef018fe6863500d7
Eva Nedeliakova <eva.nedeliakova@ui42.sk>
2021-05-06 19:10:57 +0200

    list pods in namespace

--- deleteRunPods.py
+++ deleteRunPods.py
@@ -3,7 +3,15 @@ from kubernetes import client, config
 config.load_incluster_config()
 
 v1 = client.CoreV1Api()
-print("Listing pods with their IPs: ")
-ret = v1.list_pod_for_all_namespaces(watch=False)
-for i in ret.items:
+current_namespace = open("/var/run/secrets/kubernetes.io/serviceaccount/namespace").read()
+print("Listing pods in namespace ",current_namespace," their IPs:")
+
+pods = kube_client.list_namespaced_pod(current_namespace).items
+# ret = v1.list_pod_for_all_namespaces(watch=False)
+# try:
+#     api_response = v1.delete_namespaced_pod(name, namespace)
+#     print(api_response)
+# except ApiException as e:
+#     print("Exception when calling CoreV1Api->delete_namespaced_pod: %s\n" % e)
+for i in pods:
     print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
