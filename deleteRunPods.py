7dae95ce5a498268b8de6217aa7d33c40a8e5fd3
Eva Nedeliakova <eva.nedeliakova@ui42.sk>
2021-05-06 16:39:07 +0200

    add kubeconfig

--- deleteRunPods.py
+++ deleteRunPods.py
@@ -1,5 +1,7 @@
 from kubernetes import client, config
 
+config.load_kube_config()
+
 v1 = client.CoreV1Api()
 print("Listing pods with their IPs: ")
 ret = v1.list_pod_for_all_namespaces(watch=False)
