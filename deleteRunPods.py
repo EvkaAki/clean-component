63b6ce7d6018a69c52b07cee5d508534477cd88b
Eva Nedeliakova <eva.nedeliakova@ui42.sk>
2021-05-06 23:59:38 +0200

    final skeleton

--- deleteRunPods.py
+++ deleteRunPods.py
@@ -1,6 +1,5 @@
 from kubernetes import client, config
 from kubernetes.client.rest import ApiException
-
 import argparse
 import re
 
@@ -14,7 +13,7 @@ parser.add_argument('--workflow', type=str,
   help='Path of the local file containing the Workflow name.')
 args = parser.parse_args()
 workflow = args.workflow
-print(args.workflow)
+print("Workflow name: "+ str(workflow))
 try:
     pods = v1.list_namespaced_pod(namespace=current_namespace, label_selector="workflows.argoproj.io/completed=true")
 except ApiException as e:
@@ -25,9 +24,8 @@ for pod in pods.items:
     pod_names.append(pod.metadata.name)
 
 print(workflow)
-print(pod_names)
 pod_names = [pod for pod in pod_names if re.match(r"^"+str(workflow)+"-[.]*", pod)]
-print(pod_names)
+print("Pods to be removed: "+pod_names)
 for pod_name in pod_names:
     try:
         api_response = v1.delete_namespaced_pod(pod_name, current_namespace)
