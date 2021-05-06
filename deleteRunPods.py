f6444ee9b4aafff780b8c141411f71d748ce4a3f
Eva Nedeliakova <eva.nedeliakova@ui42.sk>
2021-05-06 22:29:12 +0200

    delete pods after

--- deleteRunPods.py
+++ deleteRunPods.py
@@ -14,19 +14,16 @@ print(args)
 
 workflow = args
 pods = v1.list_namespaced_pod(current_namespace).items
-pod_names = []
 for pod in pods:
     pod_names.append(pod.metadata.name)
 print(pod_names)
-# r = re.compile(".*"+workflow)
-# pods = list(filter(r.match, pods))
-
-# ret = v1.list_pod_for_all_namespaces(watch=False)
-# try:
-#     api_response = v1.delete_namespaced_pod(name, namespace)
-#     print(api_response)
-# except ApiException as e:
-#     print("Exception when calling CoreV1Api->delete_namespaced_pod: %s\n" % e)
-for i in pods:
-    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+r = re.compile(".*"+workflow)
+pods = list(filter(r.match, pods))
+pod_names = [[pod for pod in pods if re.match(r"+workflow_name+", pod)]]
+for pod_name in pod_names:
+    try:
+        api_response = v1.delete_namespaced_pod(pod_name, current_namespace)
+        print(api_response)
+    except ApiException as e:
+        print("Exception when calling CoreV1Api->delete_namespaced_pod: %s\n" % e)
 
