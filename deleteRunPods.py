2f36ff30d24834df53ea05471a8108c8438bcaca
Eva Nedeliakova <eva.nedeliakova@ui42.sk>
2021-05-08 13:49:01 +0200

    reformat

--- deleteRunPods.py
+++ deleteRunPods.py
@@ -11,23 +11,24 @@ import re
 
 
 def delete_artefacts(pod_name):
-     minio_client = Minio (
-            "minio-service.kubeflow.svc.cluster.local:9000",
-            access_key = 'minio',
-            secret_key = 'minio123',
-            secure = False
-        )
+    minio_client = Minio(
+        "minio-service.kubeflow.svc.cluster.local:9000",
+        access_key='minio',
+        secret_key='minio123',
+        secure=False
+    )
 
     buckets = minio_client.list_buckets()
+    object_names = []
+
     for bucket in buckets:
-        print('Bucket name: ',bucket.name, bucket.creation_date)
-        objects = minio_client.list_objects(bucket.name, recursive=True,start_after=None, include_user_meta=True)
-        object_names = []
+        print('Bucket name: ', bucket.name, bucket.creation_date)
+        objects = minio_client.list_objects(bucket.name, recursive=True, start_after=None, include_user_meta=True)
         for obj in objects:
             bucket_name = object_names.append(obj._object_name)
 
-    object_names = [obj for obj in object_names if re.match(r"[\w///-]*"+str(pod_name)+"[.]*", obj)]
-    print("Artefacts to be deleted: " +str(object_names)[1:-1])
+    object_names = [obj for obj in object_names if re.match(r"[\w///-]*" + str(pod_name) + "[.]*", obj)]
+    print("Artefacts to be deleted: " + str(object_names)[1:-1])
 
     for object_name in object_names:
         try:
@@ -39,20 +40,21 @@ def delete_artefacts(pod_name):
 def delete_pods(pod_name):
     workflow = pod_name.rsplit('-', 1)[0]
 
-    print("Workflow name: "+ str(workflow))
-    print("Pod name: "+ str(pod_name))
+    print("Workflow name: " + str(workflow))
+    print("Pod name: " + str(pod_name))
     config.load_incluster_config()
     v1 = client.CoreV1Api()
     current_namespace = open("/var/run/secrets/kubernetes.io/serviceaccount/namespace").read()
 
     try:
-        pods = v1.list_namespaced_pod(namespace=current_namespace, label_selector="workflows.argoproj.io/completed=true")
+        pods = v1.list_namespaced_pod(namespace=current_namespace,
+                                      label_selector="workflows.argoproj.io/completed=true")
     except ApiException as e:
         print("Exception when calling CoreV1Api->list_namespaced_pod: %s\n" % e)
 
-    pod_names = [pod.metadata.name for pod in pods.items if re.match(r"^"+str(workflow)+"-[.]*", pod.metadata.name)]
+    pod_names = [pod.metadata.name for pod in pods.items if re.match(r"^" + str(workflow) + "-[.]*", pod.metadata.name)]
 
-    print("Pods to be removed: "+str(pod_names)[1:-1] )
+    print("Pods to be removed: " + str(pod_names)[1:-1])
     for pod_name in pod_names:
         try:
             api_response = v1.delete_namespaced_pod(pod_name, current_namespace)
@@ -63,13 +65,13 @@ def delete_pods(pod_name):
 def main():
     parser = argparse.ArgumentParser(description='Find and delete run pods.')
     parser.add_argument('--pod-path', type=str,
-      help='Path of the local file containing the Pod name.')
+                        help='Path of the local file containing the Pod name.')
     args = parser.parse_args()
     pod_name = args.pod_path
 
-    #delete artefact for download pod therefore delete dataset
+    # delete artefact for download pod therefore delete dataset
     delete_artefacts(pod_name)
-    #delete all pods used in experiment before
+    # delete all pods used in experiment before
     delete_pods(pod_name)
 
 
