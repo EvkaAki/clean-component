22bc6a58fa786b4e7d0d5a13a91f57d449ef3a15
Eva Nedeliakova <eva.nedeliakova@ui42.sk>
2021-05-08 16:51:19 +0200

    edit log

--- deleteRunPods.py
+++ deleteRunPods.py
@@ -54,6 +54,7 @@ def delete_pods(pod_name):
     for pod_name in pod_names:
         try:
             api_response = v1.delete_namespaced_pod(pod_name, current_namespace)
+            print('Pod: ' +pod_name + ' removed.')
         except ApiException as e:
             print("Exception when calling CoreV1Api->delete_namespaced_pod: %s\n" % e)
 
