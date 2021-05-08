85f609cb48a617f648a13e59ee53d9470f209335
Eva Nedeliakova <eva.nedeliakova@ui42.sk>
2021-05-08 13:24:53 +0200

    fix syntax

--- deleteRunPods.py
+++ deleteRunPods.py
@@ -62,7 +62,7 @@ def main():
         try:
             api_response = v1.delete_namespaced_pod(pod_name, current_namespace)
             status = json.loads(api_response.status)
-            print(status.)
+            print(status)
         except ApiException as e:
             print("Exception when calling CoreV1Api->delete_namespaced_pod: %s\n" % e)
 
